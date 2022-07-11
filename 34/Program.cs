4e9d7b4aa55ea41bef79dfad2028952b69c4f0d8
Denis Shaymardanov <d9655352866@gmail.com>
2022-07-11 19:27:16 +0500

    added task 34

---
+++ 34/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными 
+//числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+int[] InputArray(int number)
+{
+    int[] result = new int[number];
+    for(int i=0;i<number;i++)
+    {
+        result[i]=new Random().Next(100,1000);
+    }
+    return result;
+}
+
+void OutputArray(int[] Arr)
+{
+    int N = Arr.Length;
+    for(int i=0;i<N;i++)
+    {
+        if(i==0) Console.Write("[" + Arr[i]);
+        else Console.Write("," + Arr[i]);
+    }
+    Console.Write("]");
+}
+
+int CountEven(int[] Arr)
+{
+    int N = Arr.Length;
+    int result = 0;
+    for(int i=0;i<N;i++)
+    {
+        if (Arr[i]%2==0) result++;
+    }
+    
+    return result;
+}
+
+int[] Array = InputArray(4);
+OutputArray(Array);
+Console.Write(" -> " + CountEven(Array));
