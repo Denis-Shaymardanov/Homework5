7ad5a945bd1a979132ea030e09db7ac486296069
Denis Shaymardanov <d9655352866@gmail.com>
2022-07-11 19:58:32 +0500

    added task 38

---
+++ 38/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+//максимальным и минимальным элементов массива.
+//[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+double[] InputArray(int number)
+{
+    double[] result = new double[number];
+    for(int i=0;i<number;i++)
+    {
+        result[i]=Math.Round(new Random().NextDouble(), 2);
+    }
+    return result;
+}
+
+void OutputArray(double[] Arr)
+{
+    int N = Arr.Length;
+    for(int i=0;i<N;i++)
+    {
+        if(i==0) Console.Write("[" + Arr[i]);
+        else Console.Write("; " + Arr[i]);
+    }
+    Console.Write("]");
+}
+
+double Max(double[] Array)
+{
+    double result = Array[0];
+    int count = Array.Length;
+    for(int i=1;i<count;i++)
+    { 
+        if(Array[i]>result) result = Array[i];
+    }
+    return result;
+
+}
+
+double Min(double[] Array)
+{
+    double result = Array[0];
+    int count = Array.Length;
+    for(int i=1;i<count;i++)
+    { 
+        if(Array[i]<result) result = Array[i];
+    }
+    return result;
+}
+
+double DifferenceMaxMin(double[] Array)
+{  
+    double result = Max(Array) - Min(Array);
+    return result;
+}
+
+Console.WriteLine("Введите количество элементов массива");
+int number = Convert.ToInt32(Console.ReadLine());
+double[] Array = InputArray(number);
+OutputArray(Array);
+Console.Write(" -> " + DifferenceMaxMin(Array));
